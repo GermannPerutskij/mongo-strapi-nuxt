431fb08562e463750e8446000612ed69f01b03e0
GermannPerutskij <perutskij.ga@phystech.edu>
2021-03-21 21:32:04 +0300

    mongo-strapi-nuxt

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Список
+      </h1>
+      <hr>
+      <div class="container-items">
+        <b-spinner v-if="items === null" label="Spinning"></b-spinner>
+        <div class="item" v-for="(item, idx) in items" :key="idx">
+          <div class="name">{{ `${idx + 1}. ` + item.Title }}</div>
+          <div class="price">{{ '$ ' + item.Price }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      items: null
+    }
+  },
+  mounted() {
+    this.getItems()
+  },
+  methods: {
+    getItems() {
+      this.$axios('http://localhost:1337/items').then((response) => {
+        console.log(response.data)
+        this.items = response.data
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+.name {
+  font-size: 35px;
+  text-decoration: underline;
+}
+</style>
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '38452c14-6d15-4d1a-8cc8-e520576cc41e'
+};
